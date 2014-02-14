2ebad52605971317c67a12e483497518e6077763
Val Redchenko <lazyval@gmail.com>
2014-02-14 17:02:31 +0200

    added a styling theme to google map - should probably move to config

--- frontend/app/scripts/views/map.js
+++ frontend/app/scripts/views/map.js
@@ -48,10 +48,142 @@ define([
         render: function() {
             var _that = this;
 
+            var mapDisplayStyles = [
+                  {
+                      "featureType": "administrative",
+                      "stylers": [
+                          {
+                              "visibility": "off"
+                          }
+                      ]
+                  },
+                  {
+                      "featureType": "poi",
+                      "stylers": [
+                          {
+                              "visibility": "simplified"
+                          }
+                      ]
+                  },
+                  {
+                      "featureType": "road",
+                      "stylers": [
+                          {
+                              "visibility": "simplified"
+                          }
+                      ]
+                  },
+                  {
+                      "featureType": "water",
+                      "stylers": [
+                          {
+                              "visibility": "simplified"
+                          }
+                      ]
+                  },
+                  {
+                      "featureType": "transit",
+                      "stylers": [
+                          {
+                              "visibility": "simplified"
+                          }
+                      ]
+                  },
+                  {
+                      "featureType": "landscape",
+                      "stylers": [
+                          {
+                              "visibility": "simplified"
+                          }
+                      ]
+                  },
+                  {
+                      "featureType": "road.highway",
+                      "stylers": [
+                          {
+                              "visibility": "off"
+                          }
+                      ]
+                  },
+                  {
+                      "featureType": "road.local",
+                      "stylers": [
+                          {
+                              "visibility": "on"
+                          }
+                      ]
+                  },
+                  {
+                      "featureType": "road.highway",
+                      "elementType": "geometry",
+                      "stylers": [
+                          {
+                              "visibility": "on"
+                          }
+                      ]
+                  },
+                  {
+                      "featureType": "road.arterial",
+                      "stylers": [
+                          {
+                              "visibility": "off"
+                          }
+                      ]
+                  },
+                  {
+                      "featureType": "water",
+                      "stylers": [
+                          {
+                              "color": "#5f94ff"
+                          },
+                          {
+                              "lightness": 26
+                          },
+                          {
+                              "gamma": 5.86
+                          }
+                      ]
+                  },
+                  {},
+                  {
+                      "featureType": "road.highway",
+                      "stylers": [
+                          {
+                              "weight": 0.6
+                          },
+                          {
+                              "saturation": -85
+                          },
+                          {
+                              "lightness": 61
+                          }
+                      ]
+                  },
+                  {
+                      "featureType": "road"
+                  },
+                  {},
+                  {
+                      "featureType": "landscape",
+                      "stylers": [
+                          {
+                              "hue": "#0066ff"
+                          },
+                          {
+                              "saturation": 74
+                          },
+                          {
+                              "lightness": 100
+                          }
+                      ]
+                  }
+              ];
+
             this.$el.html(this.template());
 
             var mapOptions = {
-                zoom: 6
+                zoom: 6,
+                styles: mapDisplayStyles
             };
 
             this.map = new google.maps.Map(this.$('#map').get(0), mapOptions);
